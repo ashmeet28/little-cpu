be23029b851088fa0712d91e86a80dbd6448c29a
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2023-10-08 02:00:37 -0400

    Progress

--- compiler_0/compiler.js
+++ compiler_0/compiler.js
@@ -20,12 +20,12 @@ Compiler.generateTokens = function (source_code) {
 
     function isDigit(c) {
         c = c.charCodeAt(0);
-        return c >= '0'.charCodeAt(0) && c <= '9'.charCodeAt(0)
+        return c >= '0'.charCodeAt(0) && c <= '9'.charCodeAt(0);
     }
 
     function isAplabet(c) {
         c = c.charCodeAt(0);
-        return (c >= 'a'.charCodeAt(0) && c <= 'z'.charCodeAt(0)) || (c >= 'A'.charCodeAt(0) && c <= 'Z'.charCodeAt(0)) || (c === '_'.charCodeAt(0))
+        return (c >= 'a'.charCodeAt(0) && c <= 'z'.charCodeAt(0)) || (c >= 'A'.charCodeAt(0) && c <= 'Z'.charCodeAt(0)) || (c === '_'.charCodeAt(0));
     }
 
     function isKeyword(s) {
@@ -196,16 +196,160 @@ Compiler.generateTokens = function (source_code) {
     return scanTokens();
 };
 
-Compiler.createAST = function (tokens) {
-    return tokens;
+Compiler.generateInst = function (tokens) {
+    var allInst = [];
+    var instP = 0;
+    var instPOffset = 0x10000000;
+
+    var globalVarP = 0;
+    var globalVarPOffset = 0x20010000;
+
+    var current = 0;
+
+    var funcTable = [];
+    var varTable = [];
+
+    var currentScope = 1;
+
+    function emitInst(op, p1, p2, p3) {
+        allInst.push([op, p1, p2, p3]);
+        instP += 4;
+    }
+
+    function emitInitInst() {
+        emitInst('lliu', '01', '00', '00'); // Return Value: 0x2000_0000
+        emitInst('lui', '01', '00', '20');
+
+        emitInst('lliu', '02', '00', '00'); // Global Variables: 0x2001_0000
+        emitInst('lui', '02', '01', '20');
+
+        emitInst('lliu', '03', 'ff', 'ff'); // Local Variables: 0x3fff_ffff
+        emitInst('lui', '03', 'ff', '3f');
+
+        emitInst('lliu', '08', '00', '00'); // Jump to main function
+        emitInst('lui', '08', '00', '00');
+        emitInst('jal', '00', '00', '08');
+    }
+
+    function advance() {
+        var t = tokens[current];
+        current++;
+        return t;
+    }
+
+    function peek() {
+        return tokens[current];
+    }
+
+    function parseIntLiteral(s) {
+        return parseInt(s.replace(/_/g, ''));
+    }
+
+    function varTypeCreate() {
+        return { varSize: 0, isPointer: false, pointerLevel: 0, isArray: false, arraySize: 0, };
+    }
+
+    function varInfoCreate() {
+        return { ident: '', loc: 0, scope: 0, varType: varTypeCreate(), };
+    }
+
+    function funcInfoCreate() {
+        return { ident: '', loc: 0, parm: [], retVarType: varInfoCreate(), };
+    }
+    function getVarType() {
+        var varType = varTypeCreate();
+
+        if (peek().T == 'MUL') {
+            varType.isPointer = true;
+            varType.pointerLevel = 1;
+            advance();
+            while (peek().T === 'MUL') {
+                varType.pointerLevel++;
+                advance();
+            }
+        } else if (peek().T == 'LBRACK') {
+            varType.isArray = true;
+            advance();
+            if (peek().T !== 'RBRACK') {
+                varType.arraySize = parseIntLiteral(advance().S);
+                advance();
+            }
+        }
+
+        if (peek().T === 'IDENT' && peek().S === 'int') {
+            varType.varSize = 4;
+        }
+
+        advance();
+
+        return varType;
+    }
+
+    function getVarInfo() {
+        var varInfo = varInfoCreate();
+        varInfo.ident = advance().S;
+        varInfo.varType = getVarType();
+        return varInfo;
+    }
+
+    var functionsOfCompilingTokens = {
+        'FUNC': function () {
+            advance();
+            var funcInfo = funcInfoCreate();
+
+
+            funcInfo.ident = advance().S;
+            funcInfo.loc = instP + instPOffset;
+            advance();
+
+            while (peek().T !== 'RPAREN') {
+                funcInfo.parm.push(getVarInfo());
+                if (peek().T === 'COMMA') {
+                    advance();
+                }
+            }
+
+            advance();
+
+            if (peek().T !== 'LBRACK') {
+                funcInfo.retVarType = getVarType();
+            }
+
+            funcTable.push(funcInfo);
+
+            advance();
+            advance();
+
+            currentScope++;
+        },
+        'VAR': function () {
+            advance();
+            var varInfo = getVarInfo();
+            varInfo.scope = currentScope;
+            varTable.push(varInfo);
+            advance();
+        },
+    };
+
+    function compileTokens() {
+        while (peek().T !== 'EOF') {
+            if (functionsOfCompilingTokens[peek().T] !== undefined) {
+                functionsOfCompilingTokens[peek().T]();
+            } else {
+                console.log('Unhandled token', peek());
+                advance();
+            }
+        }
+    }
+
+    emitInitInst();
+    compileTokens();
+    console.log(funcTable);
+    console.log(varTable);
+    return allInst;
 };
 
-Compiler.compileASTAndGenerateInst = function (ast) { return ast; };
-
-Compiler.generateBytecode = function (inst) { return inst; };
-
 Compiler.compile = function (source_code) {
-    var bytecode = Compiler.compileASTAndGenerateInst(Compiler.createAST(Compiler.generateTokens(source_code)));
-    console.log(bytecode);
-    return bytecode;
+    var inst = Compiler.generateInst(Compiler.generateTokens(source_code));
+    return inst;
 };
