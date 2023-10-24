f0c0f9a4acbb8ce501e102ea82014e9abd212726
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2023-10-24 14:29:52 -0400

    Major progress

---
+++ littlecompiler/littlecompiler.go
@@ -0,0 +1,467 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+const (
+	// Token Types
+
+	TT_ILLEGAL int = iota
+	TT_EOF
+	TT_NEW_LINE
+	TT_SPACE
+
+	TT_IDENT  // main
+	TT_INT    // 12345
+	TT_CHAR   // 'a'
+	TT_STRING // "abc"
+
+	TT_ADD    // +
+	TT_SUB    // -
+	TT_MUL    // *
+	TT_QUO    // /
+	TT_REM    // %
+	TT_AND    // &
+	TT_OR     // |
+	TT_XOR    // ^
+	TT_SHL    // <<
+	TT_SHR    // >>
+	TT_LAND   // &&
+	TT_LOR    // ||
+	TT_EQL    // ==
+	TT_LSS    // <
+	TT_GTR    // >
+	TT_ASSIGN // =
+	TT_NOT    // !
+	TT_NEQ    // !=
+	TT_LEQ    // <=
+	TT_GEQ    // >=
+
+	TT_LPAREN    // (
+	TT_LBRACK    // [
+	TT_LBRACE    // {
+	TT_RPAREN    // )
+	TT_RBRACK    // ]
+	TT_RBRACE    // }
+	TT_COMMA     // ,
+	TT_PERIOD    // .
+	TT_SEMICOLON // ;
+	TT_COLON     // :
+
+	TT_WHILE
+	TT_BREAK
+	TT_CONTINUE
+	TT_IF
+	TT_ELSE
+	TT_FUNC
+	TT_RETURN
+	TT_VAR
+)
+
+type TokenInfo struct {
+	tokType int
+	tokStr  string
+}
+
+var (
+	OP_NOP   byte = 1
+	OP_ECALL byte = 2
+
+	OP_ADD byte = 4
+	OP_SUB byte = 5
+	OP_XOR byte = 6
+	OP_OR  byte = 7
+	OP_AND byte = 8
+	OP_SR  byte = 9
+	OP_SL  byte = 10
+
+	OP_PUSH_LITERAL byte = 16
+	OP_PUSH_LOCAL   byte = 17
+	OP_PUSH_GLOBAL  byte = 18
+
+	OP_POP_LITERAL byte = 20
+	OP_POP_LOCAL   byte = 21
+	OP_POP_GLOBAL  byte = 22
+
+	OP_EQ byte = 24
+	OP_NE byte = 25
+	OP_LT byte = 26
+	OP_GE byte = 27
+
+	OP_JUMP   byte = 28
+	OP_CALL   byte = 29
+	OP_RETURN byte = 30
+)
+
+func GenerateToken(src []byte) (TokenInfo, int) {
+	var bytesConsumed int
+
+	var currTok TokenInfo
+	currTok.tokType = TT_ILLEGAL
+
+	if len(src) == 0 {
+		currTok.tokType = TT_EOF
+		bytesConsumed = 0
+		return currTok, bytesConsumed
+	} else if src[0] == 0x0a {
+		currTok.tokType = TT_NEW_LINE
+		bytesConsumed = 1
+		return currTok, bytesConsumed
+	} else if src[0] == 0x20 {
+		currTok.tokType = TT_SPACE
+		bytesConsumed = 1
+		return currTok, bytesConsumed
+	}
+
+	var srcStr string
+
+	for i, c := range src {
+		if c == 0x0a {
+			srcStr = string(src[:i])
+			break
+		}
+	}
+
+	if len(srcStr) == 0 {
+		srcStr = string(src)
+	}
+
+	TokensStrings := map[int]string{
+		TT_ADD:    "+",
+		TT_SUB:    "-",
+		TT_MUL:    "*",
+		TT_QUO:    "/",
+		TT_REM:    "%",
+		TT_AND:    "&",
+		TT_OR:     "|",
+		TT_XOR:    "^",
+		TT_SHL:    "<<",
+		TT_SHR:    ">>",
+		TT_LAND:   "&&",
+		TT_LOR:    "||",
+		TT_EQL:    "==",
+		TT_LSS:    "<",
+		TT_GTR:    ">",
+		TT_ASSIGN: "=",
+		TT_NOT:    "!",
+		TT_NEQ:    "!=",
+		TT_LEQ:    "<=",
+		TT_GEQ:    ">=",
+
+		TT_LPAREN:    "(",
+		TT_LBRACK:    "[",
+		TT_LBRACE:    "{",
+		TT_RPAREN:    ")",
+		TT_RBRACK:    "]",
+		TT_RBRACE:    "}",
+		TT_COMMA:     ",",
+		TT_PERIOD:    ".",
+		TT_SEMICOLON: ";",
+		TT_COLON:     ":",
+
+		TT_WHILE:    "while",
+		TT_BREAK:    "break",
+		TT_CONTINUE: "continue",
+		TT_IF:       "if",
+		TT_ELSE:     "else",
+		TT_FUNC:     "func",
+		TT_RETURN:   "return",
+		TT_VAR:      "var",
+	}
+
+	for tokType, tokStr := range TokensStrings {
+		if len(srcStr) >= len(tokStr) && srcStr[:len(tokStr)] == tokStr {
+			if currTok.tokType == TT_ILLEGAL || len(currTok.tokStr) < len(tokStr) {
+				currTok.tokType = tokType
+				currTok.tokStr = tokStr
+				bytesConsumed = len(tokStr)
+			}
+		}
+	}
+
+	if currTok.tokType != TT_ILLEGAL {
+		return currTok, bytesConsumed
+	}
+
+	isDigit := func(c byte) bool {
+		return c >= 0x30 && c <= 0x39
+	}
+
+	isAplabet := func(c byte) bool {
+		return (c >= 0x41 && c <= 0x5a) || (c >= 0x61 && c <= 0x7a) || (c == 0x5f)
+	}
+
+	var i int = 0
+
+	if isAplabet(srcStr[i]) {
+
+		currTok.tokType = TT_IDENT
+		for (i < len(srcStr)) && (isAplabet(srcStr[i]) || isDigit(srcStr[i])) {
+			i++
+		}
+		currTok.tokStr = srcStr[:i]
+		bytesConsumed = len(srcStr[:i])
+
+	} else if isDigit(srcStr[i]) {
+
+		currTok.tokType = TT_INT
+		for (i < len(srcStr)) && (isAplabet(srcStr[i]) || isDigit(srcStr[i])) {
+			i++
+		}
+		currTok.tokStr = srcStr[:i]
+		bytesConsumed = len(srcStr[:i])
+
+	}
+
+	return currTok, bytesConsumed
+}
+
+func GenerateTokens(src []byte) []TokenInfo {
+	var toks []TokenInfo
+	var currTok TokenInfo
+	currTok.tokType = TT_ILLEGAL
+	var bytesConsumed int = 0
+
+	for currTok.tokType != TT_EOF {
+		currTok, bytesConsumed = GenerateToken(src)
+		if currTok.tokType == TT_ILLEGAL {
+			fmt.Println("Error while tokenizing")
+			os.Exit(1)
+		}
+		src = src[bytesConsumed:]
+		if currTok.tokType != TT_SPACE {
+			toks = append(toks, currTok)
+		}
+	}
+
+	return toks
+}
+
+func GenerateBytecode(toks []TokenInfo) []byte {
+	type VarInfo struct {
+		ident   string
+		varType int
+		scope   int
+		addr    int
+	}
+
+	const (
+		VT_ILLEGAL int = iota
+		VT_FUNC
+		VT_INT
+	)
+
+	const (
+		BT_ILLEGAL int = iota
+		BT_FUNC
+	)
+
+	var varTable []VarInfo
+
+	var GLOBAL_SCOPE int = 1
+	var currScope int = GLOBAL_SCOPE
+
+	var blankLiteralAddr []int
+
+	var allBytes []byte
+
+	emitByte := func(b byte) {
+		allBytes = append(allBytes, b)
+	}
+
+	emitPushLiteral := func(lit int) {
+		for ((len(allBytes) + 1) % 4) != 0 {
+			emitByte(OP_NOP)
+		}
+
+		emitByte(OP_PUSH_LITERAL)
+
+		allBytes = append(allBytes, byte(lit&0xff))
+		allBytes = append(allBytes, byte((lit>>8)&0xff))
+		allBytes = append(allBytes, byte((lit>>16)&0xff))
+		allBytes = append(allBytes, byte((lit>>24)&0xff))
+	}
+
+	emitPushBlankLiteral := func() {
+		var lit int = 0
+
+		for ((len(allBytes) + 1) % 4) != 0 {
+			emitByte(OP_NOP)
+		}
+
+		emitByte(OP_PUSH_LITERAL)
+
+		blankLiteralAddr = append(blankLiteralAddr, len(allBytes))
+
+		allBytes = append(allBytes, byte(lit&0xff))
+		allBytes = append(allBytes, byte((lit>>8)&0xff))
+		allBytes = append(allBytes, byte((lit>>16)&0xff))
+		allBytes = append(allBytes, byte((lit>>24)&0xff))
+	}
+
+	setPushBlankLiteral := func(lit int) {
+		var i int = blankLiteralAddr[len(blankLiteralAddr)-1]
+
+		blankLiteralAddr = blankLiteralAddr[:len(blankLiteralAddr)-1]
+
+		allBytes[i] = byte(lit & 0xff)
+		allBytes[i+1] = byte((lit >> 8) & 0xff)
+		allBytes[i+2] = byte((lit >> 16) & 0xff)
+		allBytes[i+3] = byte((lit >> 24) & 0xff)
+	}
+
+	findVar := func(ident string) VarInfo {
+		var varInfo VarInfo
+		varInfo.varType = VT_ILLEGAL
+
+		for _, v := range varTable {
+			if v.ident == ident {
+				varInfo = v
+				break
+			}
+		}
+
+		return varInfo
+	}
+
+	peek := func() TokenInfo {
+		return toks[0]
+	}
+
+	advance := func() TokenInfo {
+		tok := toks[0]
+		toks = toks[1:]
+		return tok
+	}
+
+	consume := func(tokType int) TokenInfo {
+		tok := toks[0]
+		if tok.tokType != tokType {
+			fmt.Println("Error while consuming token")
+			os.Exit(1)
+		}
+		toks = toks[1:]
+		return tok
+	}
+
+	getNextByteAddr := func() int {
+		return len(allBytes)
+	}
+
+	getNextLocalVarAddr := func() int {
+		var addr int = 0
+		for _, varInfo := range varTable {
+			if varInfo.scope != GLOBAL_SCOPE {
+				addr++
+			}
+		}
+		return addr
+	}
+
+	getNextGlobalVarAddr := func() int {
+		var addr int = 0
+		for _, varInfo := range varTable {
+			if varInfo.scope == GLOBAL_SCOPE {
+				addr++
+			}
+		}
+		return addr
+	}
+
+	clearLocalVarFromVarTable := func(scope int) {
+		for len(varTable) != 0 && varTable[len(varTable)-1].scope > scope {
+			varTable = varTable[:len(varTable)-1]
+		}
+	}
+
+	emitPushBlankLiteral()
+	emitByte(OP_CALL)
+	emitByte(OP_ECALL)
+
+	for peek().tokType != TT_EOF {
+		switch peek().tokType {
+		case TT_FUNC:
+			consume(TT_FUNC)
+
+			var currVarInfo VarInfo
+			currVarInfo.ident = consume(TT_IDENT).tokStr
+			currVarInfo.varType = VT_FUNC
+			currVarInfo.scope = currScope
+			currVarInfo.addr = getNextByteAddr()
+
+			varTable = append(varTable, currVarInfo)
+
+			consume(TT_LPAREN)
+			consume(TT_RPAREN)
+			consume(TT_LBRACE)
+			consume(TT_NEW_LINE)
+
+			currScope++
+
+		case TT_VAR:
+			consume(TT_VAR)
+
+			var currVarInfo VarInfo
+			currVarInfo.ident = consume(TT_IDENT).tokStr
+			currVarInfo.varType = VT_INT
+			currVarInfo.scope = currScope
+
+			if currVarInfo.scope == GLOBAL_SCOPE {
+				currVarInfo.addr = getNextGlobalVarAddr()
+			} else {
+				currVarInfo.addr = getNextLocalVarAddr()
+				emitPushLiteral(0)
+			}
+
+			consume(TT_NEW_LINE)
+
+		case TT_IDENT:
+			var varInfo = findVar(consume(TT_IDENT).tokStr)
+			consume(TT_ASSIGN)
+			emitPushLiteral(varInfo.addr)
+			v, _ := strconv.ParseInt(consume(TT_INT).tokStr, 0, 64)
+			emitPushLiteral(int(v))
+			emitByte(OP_POP_LOCAL)
+			consume(TT_NEW_LINE)
+
+		case TT_RBRACE:
+
+			currScope = GLOBAL_SCOPE
+			clearLocalVarFromVarTable(currScope)
+
+			emitByte(OP_RETURN)
+
+			consume(TT_RBRACE)
+
+		case TT_NEW_LINE:
+			advance()
+
+		default:
+			fmt.Println("Error while compiling")
+			os.Exit(1)
+		}
+	}
+
+	setPushBlankLiteral(int(findVar("main").addr))
+
+	return allBytes
+}
+
+func main() {
+	data, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	toks := GenerateTokens(data)
+
+	byteCode := GenerateBytecode(toks)
+
+	fmt.Println(byteCode)
+
+	os.WriteFile(os.Args[2], byteCode, 0666)
+}
--- littlevm/littlevm.go
+++ littlevm/littlevm.go
@@ -18,80 +18,116 @@ const (
 type VMState struct {
 	pc       uint32
 	byteCode []byte
-	sp       int
-	fp       int
+	sp       uint32
+	fp       uint32
 	s        []uint32
 	g        []uint32
-	rasp     int
-	ras      []uint32
+	rsp      uint32
+	rs       []uint32
 	status   int
 }
 
 func VMExecInst(vm VMState) VMState {
 	var (
-		NOP   byte = 1
-		ECALL byte = 2
+		OP_NOP   byte = 1
+		OP_ECALL byte = 2
 
-		ADD byte = 4
-		SUB byte = 5
-		XOR byte = 6
-		OR  byte = 7
-		AND byte = 8
-		SR  byte = 9
-		SL  byte = 10
+		OP_ADD byte = 4
+		OP_SUB byte = 5
+		OP_XOR byte = 6
+		OP_OR  byte = 7
+		OP_AND byte = 8
+		OP_SR  byte = 9
+		OP_SL  byte = 10
 
-		PUSH_LITERAL byte = 16
-		PUSH_LOCAL   byte = 17
-		PUSH_GLOBAL  byte = 18
+		OP_PUSH_LITERAL byte = 16
+		OP_PUSH_LOCAL   byte = 17
+		OP_PUSH_GLOBAL  byte = 18
 
-		POP_LITERAL byte = 20
-		POP_LOCAL   byte = 21
-		POP_GLOBAL  byte = 22
+		OP_POP_LITERAL byte = 20
+		OP_POP_LOCAL   byte = 21
+		OP_POP_GLOBAL  byte = 22
 
-		EQ byte = 24
-		NE byte = 25
-		LT byte = 26
-		GE byte = 27
+		OP_EQ byte = 24
+		OP_NE byte = 25
+		OP_LT byte = 26
+		OP_GE byte = 27
 
-		JUMP   byte = 28
-		CALL   byte = 29
-		RETURN byte = 30
+		OP_JUMP   byte = 28
+		OP_CALL   byte = 29
+		OP_RETURN byte = 30
 	)
 
 	var op byte = vm.byteCode[vm.pc]
 
 	switch op {
-	case NOP:
-	case ECALL:
+	case OP_NOP:
+		vm.pc++
 
-	case ADD:
-	case SUB:
-	case XOR:
-	case OR:
-	case AND:
-	case SR:
-	case SL:
+	case OP_ECALL:
+		vm.pc++
+		vm.status = VM_STATUS_HALT
 
-	case PUSH_LITERAL:
-	case PUSH_LOCAL:
-	case PUSH_GLOBAL:
+	case OP_ADD:
+	case OP_SUB:
+	case OP_XOR:
+	case OP_OR:
+	case OP_AND:
+	case OP_SR:
+	case OP_SL:
 
-	case POP_LITERAL:
-	case POP_LOCAL:
-	case POP_GLOBAL:
+	case OP_PUSH_LITERAL:
+		vm.pc++
 
-	case EQ:
-	case NE:
-	case LT:
-	case GE:
+		vm.s[vm.sp] = uint32(vm.byteCode[vm.pc]) | (uint32(vm.byteCode[vm.pc+1]) << 8) | (uint32(vm.byteCode[vm.pc+2]) << 16) | (uint32(vm.byteCode[vm.pc+3]) << 24)
+		vm.sp++
+		vm.pc += 4
 
-	case JUMP:
-	case CALL:
-	case RETURN:
+	case OP_PUSH_LOCAL:
+	case OP_PUSH_GLOBAL:
+
+	case OP_POP_LITERAL:
+	case OP_POP_LOCAL:
+		vm.s[vm.s[vm.sp-2]+vm.fp] = vm.s[vm.sp-1]
+		vm.sp -= 2
+		vm.pc++
+
+	case OP_POP_GLOBAL:
+
+	case OP_EQ:
+	case OP_NE:
+	case OP_LT:
+	case OP_GE:
+
+	case OP_JUMP:
+		vm.pc = vm.s[vm.sp-1]
+		vm.sp--
+
+	case OP_CALL:
+		vm.rs[vm.rsp] = vm.pc + 1
+		vm.rsp++
+
+		vm.rs[vm.rsp] = vm.fp
+		vm.rsp++
+
+		vm.pc = vm.s[vm.sp-1]
+		vm.sp--
+		vm.fp = vm.sp
+
+	case OP_RETURN:
+		vm.sp = vm.fp
+
+		vm.fp = vm.rs[vm.rsp-1]
+		vm.rsp--
+		vm.pc = vm.rs[vm.rsp-1]
+		vm.rsp--
 
 	default:
 		vm.status = VM_STATUS_ERROR
 	}
+
+	fmt.Println(vm, op)
+	fmt.Println("-----")
 
 	return vm
 }
@@ -103,11 +139,13 @@ func VMRun(vm VMState) {
 
 	for vm.status == VM_STATUS_RUNNING {
 		vm = VMExecInst(vm)
+
 		if vm.status == VM_STATUS_ERROR {
 			fmt.Println("VM STATUS: ERROR")
 		} else if vm.status == VM_STATUS_HALT {
 			fmt.Println("VM STATUS: HALT")
 		}
+
 		if len(os.Args) > 2 && os.Args[2] == "--debug" {
 			time.Sleep(100 * time.Millisecond)
 		}
@@ -123,20 +161,20 @@ func VMCreate(byteCode []byte) VMState {
 
 	vm.sp = 0
 	vm.fp = 0
-	vm.s = make([]uint32, 16777216)
+	vm.s = make([]uint32, 16)
 
-	vm.g = make([]uint32, 16777216)
+	vm.g = make([]uint32, 16)
 
-	vm.rasp = 0
-	vm.ras = make([]uint32, 65536)
+	vm.rsp = 0
+	vm.rs = make([]uint32, 16)
 
 	vm.status = VM_STATUS_READY
 
 	return vm
 }
 
-func GetByteCode(f string) []byte {
-	data, err := os.ReadFile(f)
+func GetByteCode(p string) []byte {
+	data, err := os.ReadFile(p)
 	if err != nil {
 		log.Fatal(err)
 	}
